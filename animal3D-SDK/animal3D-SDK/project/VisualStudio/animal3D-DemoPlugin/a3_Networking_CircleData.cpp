64d7a2737c75e9b3292e760adbf5670b7b9c6f73
Cameron Belcher <cameron.belcher@mymail.champlain.edi>
2020-02-15 11:43:01 -0500

    circle data we will be sending to the server

---
+++ animal3D-SDK/animal3D-SDK/project/VisualStudio/animal3D-DemoPlugin/a3_Networking_CircleData.cpp
@@ -0,0 +1,5 @@
+
+#include "a3_Networking_CircleData.h"
+
+// we dont really need anything here since it only contains the struct we 
+// will be using for networking
---
+++ animal3D-SDK/animal3D-SDK/project/VisualStudio/animal3D-DemoPlugin/a3_Networking_CircleData.h
@@ -0,0 +1,27 @@
+
+#include <vector>
+
+#ifndef CIRCLE_DATA_H
+#define CIRCLE_DATA_H
+
+struct Color
+{
+	int red;
+	int green;
+	int blue;
+	int alpha;
+};
+
+#pragma pack(push,1)
+struct CircleData
+{
+	int mouseXPos;
+	int mouseYPos;
+	int sizeCircle;
+	
+	Color circleColor;
+	
+};
+#pragma pack(pop)
+
+#endif
