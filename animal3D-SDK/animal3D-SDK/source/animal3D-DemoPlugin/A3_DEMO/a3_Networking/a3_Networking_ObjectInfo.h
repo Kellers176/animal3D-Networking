14045fd6decdf20a06a9cb017abbb88ccc2a178e
Cameron Belcher <cameron.belcher@mymail.champlain.edi>
2020-02-15 16:31:31 -0500

    new info for object we are going for... moving something.h and .cpp to right folder

---
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Networking/a3_Networking_ObjectInfo.h
@@ -0,0 +1,15 @@
+
+
+#ifndef OBJECT_INFO_H
+#define OBJECT_INFO_H
+
+#pragma pack(push, 1)
+struct ObjectMoveInfo
+{
+	float xMoveVal;
+	float yMoveVal;
+	float zMoveVal;
+};
+#pragma pack(push)
+
+#endif
