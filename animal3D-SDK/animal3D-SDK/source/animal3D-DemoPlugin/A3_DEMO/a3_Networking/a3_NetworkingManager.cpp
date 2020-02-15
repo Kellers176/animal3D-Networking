a2b164dc46e7a9fd10b43e968a316c2242867539
Cameron Belcher <cameron.belcher@mymail.champlain.edi>
2020-02-15 12:34:14 -0500

    for whatever reason... the .h file still doesnt exist for visual studio so im just putting the struct in the network manager and gonna call it there

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Networking/a3_NetworkingManager.cpp
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_Networking/a3_NetworkingManager.cpp
@@ -30,8 +30,28 @@
 #include "RakNet/RakNetTypes.h"
 #include "RakNet/BitStream.h"
 #include "RakNet/GetTime.h"
-#include "../a3_Networking_CircleData.h"
 
+// we couldnt make a file for these for whatever reason so network 
+// manager has to live with its mistakes
+struct Color
+{
+	int red;
+	int green;
+	int blue;
+	int alpha;
+};
+
+#pragma pack(push,1)
+struct CircleData
+{
+	int mouseXPos;
+	int mouseYPos;
+	int sizeCircle;
+
+	Color circleColor;
+
+};
+#pragma pack(pop)
 
 //-----------------------------------------------------------------------------
 // networking stuff
