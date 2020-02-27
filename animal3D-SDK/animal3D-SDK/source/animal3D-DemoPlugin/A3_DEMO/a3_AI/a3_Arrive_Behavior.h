6d41168f5e54d8790923aa0067a09a0a011fff54
cameron.belcher <cameron.belcher@mymail.champlain.edu>
2020-02-27 08:42:10 -0500

    got rid of comment

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_AI/a3_Arrive_Behavior.h
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_AI/a3_Arrive_Behavior.h
@@ -81,55 +81,8 @@ public:
 		steering->angular = 0;
 		return steering;
 	}
-	/*
-		def getSteering(target)
-		{
-			// create teh structure to hold our output
-			// steering = new SteeringOutput();
 
-			// get the direction to the target
-			// direction = target.position - character.position
-			// distance = direction.length();
 
-			// check if we are there, return no steering
-			// if distance < targetRadius
-			// {
-			//		return;
-			// }
-
-			// if we are outside the slowRadius, the we go to max speed
-			// if distance > slowRadius
-			// {
-			//		targetSpeed = maxSpeed
-			// }
-			// else 
-			// {
-			//		targetSpeed = maxSpeed * distance / slowRadius
-			// }
-
-			// the target velocity combines speed and direction
-			// targetVelocity = direction 
-			// targetVelocity.normalize()
-			// targetVelocity *= targetSpeed
-
-			// acceleration tries to get to the target velocity
-			// steering.linear = targetVelocity - character.velocity
-			// steering.linear /= timeToTarget
-
-			// check if the acceleration is too fast
-			// if steering.linear.length() > maxAcceleration
-			// {
-			//		steering.linear.normalize()
-			//		steering.linear *= maxAcceleration
-			// }
-			
-			// output the steering 
-			// steering.angular = 0
-			// return steering
-		}
-	*/
-
-private:
 
 };
 
