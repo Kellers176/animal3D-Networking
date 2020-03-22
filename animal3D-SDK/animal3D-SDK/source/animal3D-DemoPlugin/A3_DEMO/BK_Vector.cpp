d64979b2db17a071a6d25617469820396b782844
cameron.belcher <cameron.belcher@mymail.champlain.edu>
2020-03-22 14:28:03 -0400

    linker errors fixed with cpp file

---
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/BK_Vector.cpp
@@ -0,0 +1,71 @@
+#include "BK_Vector.h"
+
+BK_Vector2 operator+(BK_Vector2 left, BK_Vector2 right)
+{
+	BK_Vector2 newVec2(0, 0);
+
+	newVec2.xVal = left.xVal + right.xVal;
+	newVec2.yVal = left.yVal + right.yVal;
+
+	return newVec2;
+}
+
+BK_Vector2 operator+=(BK_Vector2 left, BK_Vector2 right)
+{
+	BK_Vector2 newVec2(0, 0);
+
+	newVec2.xVal = left.xVal + right.xVal;
+	newVec2.yVal = left.yVal + right.yVal;
+
+	return newVec2;
+}
+
+BK_Vector2 operator-(BK_Vector2 left, BK_Vector2 right)
+{
+	BK_Vector2 newVec2(0, 0);
+
+	newVec2.xVal = left.xVal - right.xVal;
+	newVec2.yVal = left.yVal - right.yVal;
+
+	return newVec2;
+}
+
+BK_Vector2 operator-=(BK_Vector2 left, BK_Vector2 right)
+{
+	BK_Vector2 newVec2(0, 0);
+
+	newVec2.xVal = left.xVal - right.xVal;
+	newVec2.yVal = left.yVal - right.yVal;
+
+	return newVec2;
+}
+
+BK_Vector2 operator/(BK_Vector2 left, float right)
+{
+	BK_Vector2 newVec2(0, 0);
+
+	newVec2.xVal = left.xVal / right;
+	newVec2.yVal = left.yVal / right;
+
+	return newVec2;
+}
+
+BK_Vector2 operator*(BK_Vector2 left, float right)
+{
+	BK_Vector2 newVec2(0, 0);
+
+	newVec2.xVal = left.xVal * right;
+	newVec2.yVal = left.yVal * right;
+
+	return newVec2;
+}
+
+BK_Vector2 operator*(float left, BK_Vector2 right)
+{
+	BK_Vector2 newVec2(0, 0);
+
+	newVec2.xVal = right.xVal * left;
+	newVec2.yVal = right.yVal * left;
+
+	return newVec2;
+}
--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/BK_Vector.h
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/BK_Vector.h
@@ -50,75 +50,19 @@ struct BK_Vector2
 	}
 };
 
-BK_Vector2 operator+(BK_Vector2 left, BK_Vector2 right)
-{
-	BK_Vector2 newVec2(0, 0);
+BK_Vector2 operator+(BK_Vector2 left, BK_Vector2 right);
 
-	newVec2.xVal = left.xVal + right.xVal;
-	newVec2.yVal = left.yVal + right.yVal;
+BK_Vector2 operator+=(BK_Vector2 left, BK_Vector2 right);
 
-	return newVec2;
-}
+BK_Vector2 operator-(BK_Vector2 left, BK_Vector2 right);
 
-BK_Vector2 operator+=(BK_Vector2 left, BK_Vector2 right)
-{
-	BK_Vector2 newVec2(0, 0);
+BK_Vector2 operator-=(BK_Vector2 left, BK_Vector2 right);
 
-	newVec2.xVal = left.xVal + right.xVal;
-	newVec2.yVal = left.yVal + right.yVal;
+BK_Vector2 operator/(BK_Vector2 left, float right);
 
-	return newVec2;
-}
+BK_Vector2 operator*(BK_Vector2 left, float right);
 
-BK_Vector2 operator-(BK_Vector2 left, BK_Vector2 right)
-{
-	BK_Vector2 newVec2(0, 0);
-
-	newVec2.xVal = left.xVal - right.xVal;
-	newVec2.yVal = left.yVal - right.yVal;
-
-	return newVec2;
-}
-
-BK_Vector2 operator-=(BK_Vector2 left, BK_Vector2 right)
-{
-	BK_Vector2 newVec2(0, 0);
-
-	newVec2.xVal = left.xVal - right.xVal;
-	newVec2.yVal = left.yVal - right.yVal;
-
-	return newVec2;
-}
-
-BK_Vector2 operator/(BK_Vector2 left, float right)
-{
-	BK_Vector2 newVec2(0, 0);
-
-	newVec2.xVal = left.xVal / right;
-	newVec2.yVal = left.yVal / right;
-
-	return newVec2;
-}
-
-BK_Vector2 operator*(BK_Vector2 left, float right)
-{
-	BK_Vector2 newVec2(0, 0);
-
-	newVec2.xVal = left.xVal * right;
-	newVec2.yVal = left.yVal * right;
-
-	return newVec2;
-}
-
-BK_Vector2 operator*(float left, BK_Vector2 right)
-{
-	BK_Vector2 newVec2(0, 0);
-
-	newVec2.xVal = right.xVal * left;
-	newVec2.yVal = right.yVal * left;
-
-	return newVec2;
-}
+BK_Vector2 operator*(float left, BK_Vector2 right);
 
 
 #endif // !MY_VECTOR_H
